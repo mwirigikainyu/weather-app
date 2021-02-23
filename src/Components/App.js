ba7fc18e8b242f0e8c3a7f1c9fc2655c54e1c96b
mwirigikainyu <mwirigikainyu@gmail.com>
2021-02-23 23:49:49 +0300

    UI redesign

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from "react";
-import "./App.css";
-import Conditions from "./Conditions";
+import styled from "styled-components"
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      location: "",
-    };
-  }
+  state = {
+    location: "",
+  };
 
   render() {
     const handleInputDisplay = () => {
       const apiKey = "e5b167da4cd6935ce06b32d85105271b";
       fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${this.state.location}&appid=${apiKey}`
+        `https://api.openweathermap.org/data/2.5/weather?q=${this.state.location}&units=metric&appid=${apiKey}`
       )
         .then((res) => res.json())
         .then((data) => {
           this.setState({
             weather: data,
             cod: data.cod,
+            weatherId: data.weather[0].id,
+            iconUrl: data.weather[0].icon,
           });
         });
       reset();
@@ -30,14 +28,33 @@ export default class App extends Component {
       document.querySelector("input").value = "";
     };
 
+    const link = `http://openweathermap.org/img/wn/${this.state.iconUrl}@4x.png`;
+
+
     return (
-      <div>
-        <div className="main-header">
-          <h1>WEATHER APP</h1>
-          <div className="searchBar">
+      <Wrapper>
+        <Header>
+          <Logo>
+            <p>weather.app</p>
+            <span>{new Date().toDateString()}</span>
+          </Logo>
+          {this.state.cod === 200 &&
+            <Weather>
+              <h1>{this.state.weather.name}, {this.state.weather.sys.country} </h1>
+              <img src={link} alt="icon" />
+              <div>
+                <p>{Math.floor(this.state.weather.main.temp)}°C </p>
+                <p>{this.state.weather.weather[0].description}</p>
+              </div>
+            </Weather>
+          }
+        </Header>
+
+        <Panel>
+          <Search>
             <input
               type="text"
-              placeholder="Enter City"
+              placeholder="Enter City..."
               onChange={(e) => {
                 this.setState({
                   location: e.target.value,
@@ -48,12 +65,100 @@ export default class App extends Component {
                   handleInputDisplay();
                 }
               }}
-            ></input>
-            <button onClick={handleInputDisplay}>SEARCH</button>
-          </div>
-        </div>
-        <Conditions data={this.state.weather} cod={this.state.cod} />
-      </div>
+            />
+          </Search>
+          <Cities>
+            <p>Nairobi</p>
+            <p>Amsterdam</p>
+            <p>Manchester</p>
+            <p>Kigali</p>
+            <p>Pretoria</p>
+          </Cities>
+          <h2>Weather Details</h2>
+          {this.state.cod === 200 &&
+            <Details>
+              <li>Humidity: <span>{this.state.weather.main.humidity}%</span></li>
+              <li>Clouds: <span>{this.state.weather.clouds.all}%</span></li>
+              <li>Pressure: <span>{this.state.weather.main.pressure}hPa</span></li>
+              <li>Wind: <span>{this.state.weather.wind.speed}m/s</span></li>
+            </Details>
+          }
+        </Panel>
+      </Wrapper>
     );
   }
 }
+
+const Wrapper = styled.div`
+display: flex;
+justify-content: space-between;
+`;
+
+const Header = styled.section`
+margin: 50px;
+max-height: 800px;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+`;
+
+const Panel = styled.section`
+background-color: rgba(255, 255, 255, .15);  
+ backdrop-filter: blur(5px);
+width: 40%;
+min-height: 100vh;
+color: grey;
+padding: 50px;
+h2{
+border-top: 2px solid rgba(255, 255, 255, .25);
+padding-top: 30px;
+}
+`;
+
+const Logo = styled.h2`
+font-weight: 100;
+p{
+  padding-bottom: 10px;
+  text-decoration: underline;
+}
+`;
+const Details = styled.ul`
+margin: 50px 0;
+li{
+  display: flex;
+  justify-content: space-between;
+  font-size: 25px;
+  list-style: none;
+  padding: 20px 0;
+}
+`;
+
+const Search = styled.div`
+text-align: center;
+`;
+
+const Cities = styled.div`
+color: #5a5a5a;
+font-size: 20px;
+margin: 30px;
+p{
+  padding: 20px 50px;
+}
+`;
+
+const Weather = styled.div`
+display: flex;
+align-items: center;
+justify-content: flex-end;
+border-bottom: 1px solid white;
+text-align: center;
+h1{
+  font-size: 120px; 
+}
+img{
+  margin: 0 20px;
+}
+p{
+  font-size: 40px;
+}
+`;
